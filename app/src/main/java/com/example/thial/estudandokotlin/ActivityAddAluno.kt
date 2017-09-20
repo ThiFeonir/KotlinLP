fa02d8b96a028537254a342ac4446a7e60c6804d
WeslleyDeveloper <weslley_campos@outlook.com>
2017-09-19 23:02:20 -0300

    Instanciando elemento da classe addAluno!

--- app/src/main/java/com/example/thial/estudandokotlin/ActivityAddAluno.kt
+++ app/src/main/java/com/example/thial/estudandokotlin/ActivityAddAluno.kt
@@ -2,11 +2,17 @@ package com.example.thial.estudandokotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_add_aluno.*
 
 class ActivityAddAluno : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_aluno)
+
+        var nome = findViewById(R.id.mEdtNome) as EditText
+        var matricular = findViewById(R.id.mEdtMatricula) as EditText
+
     }
 }
