35158fd6b2ec1a22154f0edeaf1da9f04dcba3a5
Weslley Campos <weslley_campos@outlook.com>
2017-09-22 18:22:46 -0300

    Erros relacionados a arquivos.

--- app/src/main/java/com/example/thial/estudandokotlin/MainActivity.kt
+++ app/src/main/java/com/example/thial/estudandokotlin/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.thial.estudandokotlin
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
+import java.io.IOException
+import java.io.ObjectInputStream
 
 class MainActivity : AppCompatActivity() {
+
+    val turma = Turma()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +19,25 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.setTitle(R.string.app_name)
 
+        val turminha : Turma = this.abrirArquivo()
+
         val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener { view ->
-            /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()*/
-            val i = Intent(this,ActivityExibirDisciplinas::class.java)
+            val i = Intent(this,ActivityAddAluno::class.java)
+            i.putExtra("turma", turminha)
             startActivity(i)
         }
     }
 
+    @Throws(IOException::class, ClassNotFoundException::class)
+    fun abrirArquivo(): Turma {
+
+        val fis = this.applicationContext .openFileInput("turma.dat")
+        val ois = ObjectInputStream(fis)
+
+        return ois.readObject() as Turma
+    }
+
 }
+/*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                  .setAction("Action", null).show()*/
