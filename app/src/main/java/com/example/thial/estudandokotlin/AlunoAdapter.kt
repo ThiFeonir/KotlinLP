aaec4f3c39215837b20e935bde6bddd728ec2590
WeslleyDeveloper <weslley_campos@outlook.com>
2017-09-21 21:27:57 -0300

    Novo layout para o recycler view.

--- app/src/main/java/com/example/thial/estudandokotlin/AlunoAdapter.kt
+++ app/src/main/java/com/example/thial/estudandokotlin/AlunoAdapter.kt
@@ -1,13 +1,12 @@
 package com.example.thial.estudandokotlin
 
 import android.content.Context
-import android.content.DialogInterface
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import kotlinx.android.synthetic.main.alunos_linhas.view.*
+import kotlinx.android.synthetic.main.aluno_row.view.*
 
 /**
  * Created by Weslley on 21/09/2017.
@@ -15,7 +14,7 @@ import kotlinx.android.synthetic.main.alunos_linhas.view.*
 class AlunoAdapter(val context: Context, val alunoLista: ArrayList<Aluno>) : RecyclerView.Adapter<AlunoAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): AlunoAdapter.ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.alunos_linhas, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.aluno_row, parent, false)
         return ViewHolder(view)
     }
 
@@ -32,7 +31,7 @@ class AlunoAdapter(val context: Context, val alunoLista: ArrayList<Aluno>) : Rec
         fun bindItems(aluno: Aluno) {
             nome = aluno.nome
             itemView.mTxtAluno.text = aluno.nome
-            itemView.mTxtNumMatricula.text = aluno.matricula
+            itemView.mTxtMatricula.text = aluno.matricula
         }
 
         init {
